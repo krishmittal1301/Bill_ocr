fc24621ae3df94a42b3bb29154c5d5e4333e9cac
krishnamittal <148966133+krishmittal1301@users.noreply.github.com>
2024-07-12 09:16:03 +0530

    Update settings.py

--- bill_ocr/settings.py
+++ bill_ocr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-58pp)3c5i8al*e8w2o&ij&d7ozu9=bgwjg!k$o*ghiay!4xuii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
